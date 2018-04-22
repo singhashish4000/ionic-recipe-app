8d46e0a0ce823d3ecd16096f785d1bf97ab0c2c6
Ashish Singh <singhashish4000@gmail.com>
2018-04-22 10:54:54 +0530

    app working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { EditRecipePage } from '../pages/edit-recipe/edit-recipe';
+import { RecipePage } from '../pages/recipe/recipe';
+import { RecipesPage } from '../pages/recipes/recipes';
+import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
+import { ShoppingListServices } from '../services/shopping-list';
+import { Recipe } from '../models/recipe';
+import { RecipesService } from '../services/recipes';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    EditRecipePage,
+    RecipePage,
+    RecipesPage,
+    ShoppingListPage,
     TabsPage
   ],
   imports: [
@@ -26,14 +32,18 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    TabsPage,
+    EditRecipePage,
+    RecipePage,
+    RecipesPage,
+    ShoppingListPage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ShoppingListServices,
+    RecipesService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
